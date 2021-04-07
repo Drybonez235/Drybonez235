b4f4dae12ae16fce0238b7e88e59b93fa3453943
Drybonez235 <82049078+Drybonez235@users.noreply.github.com>
2021-04-07 19:09:12 -0400

    Update Game.py

--- Game.py
+++ Game.py
@@ -2,15 +2,17 @@ Python 3.9.2 (v3.9.2:1a79785e3e, Feb 19 2021, 09:06:10)
 [Clang 6.0 (clang-600.0.57)] on darwin
 Type "help", "copyright", "credits" or "license()" for more information.
 >>>class Warrior(object):
-	def __init__(self,name,attack,defense,health):
-		self.name=name
-		self.attack=attack
-		self.defense=defense
-		self.health=health
+class Warrior(object):
+    def __init__(self,Race,Name,Attack,Defense,Health):
+        self.Race=Race
+        self.Name=Name
+        self.Attack=Attack
+        self.Defense=Defense
+        self.Health=Health
 
-Goblin= Warrior ("Bob" ,10,15,100)
-Elf= Warrior("Elon", 5,30,100)
-Archer= Warrior("Mike", 15,0,100)
+Bob= Warrior("Goblin","Bob",15,10,100)
+Elon= Warrior("Elf","Elon",10,20,100)
+Mike= Warrior("Man","Mike",20,0,100)
 
 def Damage(Warrior):
     Warrior.attack=Warrior.attack*(randrange(1,2,.1))
@@ -20,19 +22,21 @@ def Mitigation(Warrior):
     Warrior.defense=Warrior.defense*(randrange(1,2,.1))
     return Warrior.defense
     
-def Battle(*Warrior):
+def Battle(*Warrior: object) -> object:
     Warrior1=Warrior[0]
     Warrior2=Warrior[1]
-    Battle_Damage=Damage(warrior1)
-    Battle_Mitigation=Mitigation(Warrior2)
+    Battle_Damage=(Damage(Warrior1))
+    Battle_Mitigation=(Mitigation(Warrior2))
     if Battle_Damage<Battle_Mitigation
-        Warrior2.health= (Warrior2.health)+(Battle_Mitigation-Battle_Damage)
+        Warrior2.health= (Warrior2.health+(Battle_Mitigation-Battle_Damage))
         print(str(Battle_Damage),"Damage")
-        print(str(Warrior2.name),"has",(str(Warrior2.health)),"health remaining")
-    elif Battle_Damage==Battle_Mitigation:
-        print("draw")
-    elif Battle_Damage>Battle_Mitigation:
-        Warrior1.health= (Warrior1.health)-(Battle_Mitigation-Battle_Damage)
+        print(str(Warrior2.name),"has",str(Warrior2.Health),"Health Remaning")
+    elif Battle_Damage==Battle_Mitigation
+        print("Draw")
+    elif Battle_Damage>Battle_Mitigation
+        Warrior1.Health= Warrior1.Health-(Battle_Mitigation-Battle_Damage)
+        print(str(Battle_Mitigation),"Recoil was done")
+        print(str(Warrior1.name),"has",str(Warrior2.Health),"Health Remaining"))
         
 
 def Health(Warrior):
